3f95ee43f51ad7703b48d76b0889e1508060cd13
Culaxiz Alexei <6217367@cegepmontpetit.ca>
2023-09-15 17:49:45 -0400

    FCT DI - Module, Bootstrap, MDB
    - J'ai ajouté la référence à la bibliothèque MDB
    - Injection de dépendance pour FormsModule, HttpClientModule
    - nettoyage initial du html,
    - Html pour app.component.html

--- BookFinder/src/app/app.module.ts
+++ BookFinder/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClient, HttpClientModule} from '@angular/common/http'
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
